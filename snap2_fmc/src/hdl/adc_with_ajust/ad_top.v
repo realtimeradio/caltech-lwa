6b3b8f6b53b0adf5cf2d32199476e4ec0db42ef1
Jack Hickish <jackh@maze.caltech.edu>
2020-01-29 06:33:22 -0800

    Parameterize SPI clock delay
    Set clock delay to 30 cycles of clk200 for FMC-A (as before)
    Set delay to 60 cycles of clk200 for FMC-B (which empirically works)

--- snap2_fmc/src/hdl/adc_with_ajust/ad_top.v
+++ snap2_fmc/src/hdl/adc_with_ajust/ad_top.v
@@ -218,7 +218,9 @@ wire  a_clk_data;
 wire  a_clk_data_locked;
 //--------------------------
 
-ad_fun inst_AD_u72 (
+ad_fun  #(
+    .CLK_DELAY(30)
+    ) inst_AD_u72 (
 	 //INPUT
 	//----------------
     .gclk200m_buf(clk200m), 
@@ -299,7 +301,9 @@ wire  [3:0]  b_data_or;
 wire  b_clk_data;
 wire  b_clk_data_locked;
 //--------------------------
-ad_fun inst_AD_u73 (
+ad_fun #(
+    .CLK_DELAY(60)
+    ) inst_AD_u73 (
     //INPUT 
 	//----------------
     .gclk200m_buf(clk200m), 
