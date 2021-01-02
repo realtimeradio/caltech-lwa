bcbbc7e3c804d01fdc8da86060e9e78b1ff26179
JackH <jack@realtimeradio.co.uk>
2021-01-02 10:28:54 -0800

    Update SPI script for latest casperfpga and add readonly option

--- ads5296_snap2_tests/send_spi.py
+++ ads5296_snap2_tests/send_spi.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
                         help="Snap hostname / IP address")
     parser.add_argument("--addr", type=int, default=0x2a,
                         help="Address to read/write")
-    parser.add_argument("--data", type=int, default=0xaaaa,
-                        help="Value to write")
+    parser.add_argument("--data", type=int, default=None,
+                        help="Value to write. If None is given, the address will be read and not written")
     parser.add_argument("--reset", action="store_true",
                         help="Strobe ADC reset line")
     parser.add_argument("--sync", action="store_true",
@@ -64,11 +64,15 @@ if __name__ == "__main__":
                 f.assert_sync()
                 f.deassert_sync()
             for cs in range(args.csstart, args.csstop + 1):
-                print("Writing 0x%x to address 0x%x with chip-select %d on FMC %d" % (args.data, args.addr, cs, fn))
-                f.write_spi(args.addr, args.data, cs)
-                f.enable_readout(cs)
-                readback = f.write_spi(args.addr, 0, cs)
-                f.disable_readout(cs)
+                if args.data is None:
+                    print("Reading address 0x%x with chip-select %d on FMC %d" % (args.addr, cs, fn))
+                    readback = f.read_spi(args.addr, cs)
+                else:
+                    print("Writing 0x%x to address 0x%x with chip-select %d on FMC %d" % (args.data, args.addr, cs, fn))
+                    readback = f.write_spi(args.addr, args.data, cs)
+                #f.enable_readout(cs)
+                #readback = f.write_spi(args.addr, 0, cs)
+                #f.disable_readout(cs)
                 print("Read back 0x%x" % readback)
         if not args.loop:
             break
