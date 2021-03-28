d7235c6ee98d2064567fe2e2151f9e72220f5675
JackH <jack@realtimeradio.co.uk>
2021-03-28 10:26:43 -0700

    Tweak autocorr for multiplexed core case

--- control_sw/src/blocks/autocorr.py
+++ control_sw/src/blocks/autocorr.py
@@ -6,12 +6,13 @@ from .block import Block
 
 class AutoCorr(Block):
     def __init__(self, host, name,
-                 acc_len=2**16,
+                 acc_len=2**15,
                  logger=None,
                  n_chans=4096,
                  n_pols=64,
                  n_parallel_streams=8,
                  n_cores=4,
+                 use_mux=True,
                 ):
         """
         Instantiate an correlation block, which allows correlation
@@ -25,6 +26,7 @@ class AutoCorr(Block):
             n_pols (int) : Number of polarizations processed
             n_parallel_streams (int) : Number of parallel data channels processed
             n_firmware_cores (int) : Number of firmware autocorr submodules
+            use_mux (bool) : If True, multiplex streams into a common core
         """
         super(AutoCorr, self).__init__(host, name, logger)
         self.n_chans = n_chans
@@ -32,6 +34,7 @@ class AutoCorr(Block):
         self.n_pols = n_pols
         self._n_parallel_streams = n_parallel_streams
         self._n_cores = n_cores
+        self._use_mux = use_mux
    
     def _wait_for_acc(self):
         """
@@ -41,55 +44,85 @@ class AutoCorr(Block):
         while self.read_uint('acc_cnt') < (cnt+1):
             time.sleep(0.1)
 
+    def _set_mux(self, sel):
+        """
+        Set the core input multiplexer to input `sel`
+        """
+        if not self._use_mux:
+            return
+        if sel >= self._n_cores:
+            self.logger.error("Cannot select input %d when there are only %d cores" % (sel, self._n_cores))
+            return
+        self.write_int('mux_sel', sel)
+
     def _read_bram(self):
         """ 
         Reads ram containing autocorrelation spectra for all polarizations.
         Returns:
             numpy array containing uint64 autocorrelation spectra
         """
-        dout = np.zeros([self.n_pols, self.n_chans], dtype=np.float32)
+        if self._use_mux:
+            dout = np.zeros([self.n_pols // self._n_cores, self.n_chans], dtype=np.float32)
+            read_loop_range = range(1)
+        else:
+            dout = np.zeros([self.n_pols, self.n_chans], dtype=np.float32)
+            read_loop_range = range(self._n_cores)
         n_words_per_stream = self.n_pols * self.n_chans // self._n_parallel_streams // self._n_cores
         n_pols_per_core = self.n_pols // self._n_cores
         n_chans_per_stream = self.n_chans // self._n_parallel_streams
-        for core in range(self._n_cores):
+        for core in read_loop_range:
             for stream in range(self._n_parallel_streams):
-                raw = self.read('%d_dout%d_bram' % (core, stream), 4*n_words_per_stream)
+                if self._use_mux:
+                    ramname = 'common_dout%d_bram' % (stream) # Why call it "common" rather than 0?!
+                else:
+                    ramname = '%d_dout%d_bram' % (core, stream)
+                raw = self.read(ramname, 4*n_words_per_stream)
                 x = struct.unpack('>%df' % n_words_per_stream, raw)
                 for subpol in range(self.n_pols // self._n_cores):
                     dout[core*n_pols_per_core + subpol, stream::self._n_parallel_streams] = \
                         x[subpol*n_chans_per_stream:(subpol+1)*n_chans_per_stream]
         return dout
     
-    def get_new_spectra(self):
+    def get_new_spectra(self, core_block=0):
         """
         Get a new accumulation.
+        Inputs:
+            core_block (int) : If using multiplexing, read input block ID `core_block`
         Returns: numpy array of shape(n_pol, n_chans), containing
                  autocorrelations with accumulation length divided out.
 
         """
+        if self._use_mux:
+            self._set_mux(core_block)
         self._wait_for_acc()
-        spec = self._read_bram() / float(self._acc_len)
+        spec = self._read_bram() / float(self.get_acc_len())
         return spec
 
-    def plot_spectra(self, db=True, show=True):
+    def plot_spectra(self, core_block=0, db=True, show=True):
         """
         Plot spectra of all polarizations, with accumulation length divided out
         
         Inputs:
+            core_block (int) : If using multiplexing, read input block ID `core_block`
             db (bool): If True, plot 10log10(power). Else, plot linear
             show (bool): If True, call matplotlib's `show` after plotting
         Returns: matplotlib Figure
         """
         from matplotlib import pyplot as plt
-        spec = self.get_new_spectra()
+        specs = self.get_new_spectra(core_block)
         f, ax = plt.subplots(1,1)
         if db:
             ax.set_ylabel('Power [dB]')
+            specs = 10*np.log10(specs)
         else:
             ax.set_ylabel('Power [linear]')
         ax.set_xlabel('Frequency Channel')
-        for i in range(self.n_pols):
-            ax.plot(10*np.log10(np.abs(spec[i])), label="pol_%d"%i)
+        if self._use_mux:
+            channel_offset = core_block * self.n_pols // self._n_cores
+        else:
+            channel_offset = 0
+        for speci, spec in enumerate(specs):
+            ax.plot(spec, label="pol_%d" % (channel_offset + speci))
         ax.legend()
         if show:
             plt.show()
