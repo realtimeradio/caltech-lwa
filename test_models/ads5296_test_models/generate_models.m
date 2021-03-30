97ef9b454d34cb89cef4baaeaf785822580fbc8d
Jack <jack@realtimeradio.co.uk>
2021-03-30 16:55:33 +0000

    Add first round of dual ADC FMC tests
    Regenerate by tweaking the template model, then running:
    generate_models -- to make the different clock/pinout models
    build_models -- to compile the different slx models to fpg files

---
+++ test_models/ads5296_test_models/generate_models.m
@@ -0,0 +1,26 @@
+
+template_model = 'ads5296_dual_template'
+load_system(template_model);
+filename = get_param(template_model, 'FileName');
+[filepath, name, ext] = fileparts(filename);
+for clock=[0 1]
+    for fmc_bver = [1 2]
+        load_system(template_model);
+        new_model = [filepath '/test_ads5296_fmca_r2_fmcb_r' num2str(fmc_bver) '_clk' num2str(clock)]
+        %system(['cp' ' ' filename ' ' new_model]);
+        %model_handle = load(new_model)
+        set_param([template_model '/SNAP2'], 'clk_src', ['adc' num2str(clock) '_clk']);
+        set_param([template_model '/fmca'], ...
+            'fmc_clock', num2str(clock), ...
+            'fmc_port', '0', ...
+            'board_version', '2' ...
+            );
+        set_param([template_model '/fmcb'], ...
+            'fmc_clock', num2str(clock), ...
+            'fmc_port', '1', ...
+            'board_version', num2str(fmc_bver) ...
+            );
+        save_system(template_model, new_model);
+        close_system(new_model);
+    end
+end
