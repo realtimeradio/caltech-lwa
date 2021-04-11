48119a151db4d0ac04e7c3a6fed809e1b86e2ffc
JackH <jack@realtimeradio.co.uk>
2021-04-11 10:42:10 -0700

    docstring updates

--- control_sw/src/blocks/delay.py
+++ control_sw/src/blocks/delay.py
@@ -2,16 +2,23 @@ from .block import Block
 from lwa_f.error_levels import *
 
 class Delay(Block):
-    MIN_DELAY = 5 # minimum delay allowed
+    """
+    Instantiate a control interface for a Delay block.
+
+    :param host: CasperFpga interface for host.
+    :type host: casperfpga.CasperFpga
+
+    :param name: Name of block in Simulink hierarchy.
+    :type name: str
+
+    :param logger: Logger instance to which log messages should be emitted.
+    :type logger: logging.Logger
+
+    :param n_streams: Number of independent streams which may be delayed
+    :type n_streams: int
+    """
+    MIN_DELAY = 5 #: minimum delay allowed
     def __init__(self, host, name, n_streams=64, logger=None):
-        """
-        Instantiate a delay contol block.
-        
-        Inputs:
-            host (casperfpga.CasperFpga): Host FPGA object
-            name (string): Name (in simulink) of this block
-            n_streams (int): Number of streams this block handles
-        """
         super(Delay, self).__init__(host, name, logger)
         self.n_streams = n_streams
         self.max_delay = None
@@ -21,21 +28,21 @@ class Delay(Block):
         Query the firmware to get the maximum delay it
         supports.
 
-        Returns
-        -------
-        delay : int
-            Maximum supported delay, in ADC samples
+        :return: Maximum supported delay, in ADC samples
+        :rtype: int
         """
         self.max_delay = self.read_uint('max_delay')
         return self.max_delay
 
     def set_delay(self, stream, delay):
         """
-        Insert a delay to a given input stream.
+        Set the delay for a given input stream.
 
-        Inputs:
-            stream (int): Which antpol to delay.
-            delay (int): Number of ADC clock cycles of delay to insert.
+        :param stream: ADC stream index to which delay should be applied.
+        :type stream: int
+
+        :param delay: Number of ADC clock cycles delay to load.
+        :type delay: int
         """
         if delay < self.MIN_DELAY:
             self._warning("User requested delay of %d, but choosing %d because this is the minimum" % (delay, self.MIN_DELAY))
@@ -45,6 +52,7 @@ class Delay(Block):
             self.get_max_delay()
         if delay >= self.max_delay:
             self._error('Tried to set delay to %d which is > the allowed maximum (%d)' % (delay, self.max_delay))
+            delay = self.max_delay-1
         self._info('Setting delay of stream %d to %d' % (stream, delay))
         self.write_int('%d_delay' % stream, delay)
 
@@ -52,15 +60,11 @@ class Delay(Block):
         """
         Get the current delay for a given input.
 
-        Parameters
-        ----------
-        stream : int
-            Which input number to query
+        :param stream: Which ADC input index to query
+        :type stream: int
 
-        Returns
-        -------
-        delay : int
-            Currently loaded delay, in ADC samples
+        :return: Currently loaded delay, in ADC samples
+        :rtype: int
         """
         if stream > self.n_streams:
             self._error('Tried to get delay for stream %d > n_streams (%d)' % (stream, self.n_streams))
@@ -68,7 +72,11 @@ class Delay(Block):
 
     def initialize(self, read_only=False):
         """
-        Initialize all delays to minimum allowed
+        Initialize all delays.
+
+        :param read_only: If True, do nothing. If False, initialize all
+            delays to the minimum allowed value.
+        :type read_onle: bool
         """
         self.max_delay = self.get_max_delay()
         if not read_only:
@@ -76,9 +84,25 @@ class Delay(Block):
                 self.set_delay(i, self.MIN_DELAY)
 
     def get_status(self):
+        """
+        Get status and error flag dictionaries.
+
+        Status keys:
+
+            - delay<``n``>: Currently loaded delay for ADC input index ``n``.
+            - max_delay: The maximum delay supported by the firmware.
+            - min_delay: The minimum delay supported by the firmware.
+
+        :return: (status_dict, flags_dict) tuple. `status_dict` is a dictionary of
+            status key-value pairs. flags_dict is
+            a dictionary with all, or a sub-set, of the keys in `status_dict`. The values
+            held in this dictionary are as defined in `error_levels.py` and indicate
+            that values in the status dictionary are outside normal ranges.
+`       """
         stats = {}
         flags = {}
         for i in range(self.n_streams):
             stats['delay%.2d' % i] = self.get_delay(i)
-        stats['max_delay'] = self.get_max_delay()
+        stats['max_delay'] = self.get_max_delay()-1
+        stats['min_delay'] = self.MIN_DELAY
         return stats, flags
