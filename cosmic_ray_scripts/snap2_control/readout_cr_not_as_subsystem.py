80ca7623aef6c7ab1814b289e074afc897756cfc
ubuntu <ubuntu@pm.me>
2021-08-18 16:55:42 +0000

    I made a separate readout script readout_cr_not_as_subsystem.py that will continue to work with versions of the firmware where the cosmic ray system is the whole design, and then I edited readout.py to have the new register names for the cosmic ray system as a subsystem within the f-engine.

--- cosmic_ray_scripts/snap2_control/readout_cr_not_as_subsystem.py
+++ cosmic_ray_scripts/snap2_control/readout_cr_not_as_subsystem.py
@@ -49,90 +49,90 @@ if program==True:
         time.sleep(10)
 brd.get_system_information(fpgfile)
 if destination == 'lwacr':
-    brd.write_int('cosmic_ray_cr_dest_ip',(10<<24)+(41<<16)+(0<<8)+106)
-    brd.write_int('cosmic_ray_cr_dest_port',11111)
-    brd.gbes.cosmic_ray_cr_forty_gbe.print_gbe_core_details(arp=True)
-    brd.gbes.cosmic_ray_cr_forty_gbe.configure_core(mac, ip, 11111)
-    brd.gbes.cosmic_ray_cr_forty_gbe.set_single_arp_entry('10.41.0.106',  0x043f72dfc2f8)
-    brd.gbes.cosmic_ray_cr_forty_gbe.print_gbe_core_details(arp=True)
+    brd.write_int('cr_dest_ip',(10<<24)+(41<<16)+(0<<8)+106)
+    brd.write_int('cr_dest_port',11111)
+    brd.gbes.cr_forty_gbe.print_gbe_core_details(arp=True)
+    brd.gbes.cr_forty_gbe.configure_core(mac, ip, 11111)
+    brd.gbes.cr_forty_gbe.set_single_arp_entry('10.41.0.106',  0x043f72dfc2f8)
+    brd.gbes.cr_forty_gbe.print_gbe_core_details(arp=True)
 
 elif destination == 'minor':
-    brd.write_int('cosmic_ray_cr_dest_ip',3232246028)
-    brd.write_int('cosmic_ray_cr_dest_port',11111)
-    brd.gbes.cosmic_ray_cr_forty_gbe.print_gbe_core_details(arp=True)
-    brd.gbes.cosmic_ray_cr_forty_gbe.configure_core(mac, ip, 11111)
-    brd.gbes.cosmic_ray_cr_forty_gbe.set_single_arp_entry('192.168.41.12', 0x98039b3d8b7b)
-    brd.gbes.cosmic_ray_cr_forty_gbe.print_gbe_core_details(arp=True)
+    brd.write_int('cr_dest_ip',3232246028)
+    brd.write_int('cr_dest_port',11111)
+    brd.gbes.cr_forty_gbe.print_gbe_core_details(arp=True)
+    brd.gbes.cr_forty_gbe.configure_core(mac, ip, 11111)
+    brd.gbes.cr_forty_gbe.set_single_arp_entry('192.168.41.12', 0x98039b3d8b7b)
+    brd.gbes.cr_forty_gbe.print_gbe_core_details(arp=True)
 else:
     print(destination + " is not a recognized destination.")
     print(destination=='lwacr')
 # set test data source
 if datasource=='constant':
-	#brd.write_int('cosmic_ray_select_input_signal',1)
-	brd.write_int('cosmic_ray_select_input_signal1',1)
+	brd.write_int('select_input_signal',1)
+	#brd.write_int('select_input_signal1',1)
 elif datasource=='counter':
-	#brd.write_int('cosmic_ray_select_input_signal',0)
-	brd.write_int('cosmic_ray_select_input_signal1',0)
+	brd.write_int('select_input_signal',0)
+	#brd.write_int('select_input_signal1',0)
 else:
 	print("datasource must be 'constant' or 'counter'. Defaulting to 'counter'")
 #set wait time
 if packetwait:
-	brd.write_int('cosmic_ray_wait_between_packets', packetwait)
+	brd.write_int('wait_between_packets', packetwait)
 
 ##########################Reset and then Send Trigger#####################
 
 #Ensure the board is listening: write the register reset_to_listen to 0 and then to 1. Confirm that it is reset.
-brd.write_int('cosmic_ray_reset_to_listen',0)
-brd.write_int('cosmic_ray_reset_to_listen',1)
-val = brd.read_int('cosmic_ray_readout_state_value')
+brd.write_int('reset_to_listen',0)
+brd.write_int('reset_to_listen',1)
+val = brd.read_int('readout_state_value')
 assert val==0, "board didn't reset to listen"
 
 #Reset the readout timer by putting readout_timer_reset1 to 0 and then to 1, and make sure the timer is staying zero and not counting.
-brd.write_int('cosmic_ray_readout_timer_reset1',0)
-brd.write_int('cosmic_ray_readout_timer_reset1',1)
-val = brd.read_int('cosmic_ray_readout_state_timer1')
+brd.write_int('readout_timer_reset1',0)
+brd.write_int('readout_timer_reset1',1)
+val = brd.read_int('readout_state_timer1')
 assert val == 0, "readout state timer started before trigger"
 
 #Read the readout state register: Read readout_state_value. It should be 0.
-val = brd.read_int('cosmic_ray_readout_state_value')
+val = brd.read_int('readout_state_value')
 assert val == 0, "board in readout state before trigger sent"
 
 #Read tx_count_eof_and_valid and overflow counters before sending the trigger. This counts how many packets the board thinks are sent and whether the packetizer buffer ever overflowed or almost overflowed..
-previous_packets = brd.read_int('cosmic_ray_tx_count_eof_and_valid')
+previous_packets = brd.read_int('tx_count_eof_and_valid')
 
-previous_afull = brd.read_int('cosmic_ray_forty_g_tx_afull')
-previous_overflows = brd.read_int('cosmic_ray_forty_g_tx_of')
+previous_afull = brd.read_int('forty_g_tx_afull')
+previous_overflows = brd.read_int('forty_g_tx_of')
 
 #Send a trigger from software: write register send_trigger to 0 then to 1. 
-brd.write_int('cosmic_ray_send_trigger',0)
-brd.write_int('cosmic_ray_send_trigger',1)
+brd.write_int('send_trigger',0)
+brd.write_int('send_trigger',1)
 
 #wait a second
 time.sleep(1)
 
 ##############################Read registers after the readout ###############
 #After short wait, read tx_count_eof_and_valid, and confirm that it has incremented by 64.
-packets = brd.read_int('cosmic_ray_tx_count_eof_and_valid') 
+packets = brd.read_int('tx_count_eof_and_valid') 
 print ("Packetizer formed " + str(packets - previous_packets) + " packets")
 
 #Read readout_state_value. It should be 0.
-val = brd.read_int('cosmic_ray_readout_state_value')
+val = brd.read_int('readout_state_value')
 if val == 0:
 	print("Board successfully returned to listening state after readout")
 if val !=0:
 	print("Board did not return to listening state after readout. State value = " + str(val))
 
 #Read register readout_state_timer.
-val = brd.read_int('cosmic_ray_readout_state_timer1')
+val = brd.read_int('readout_state_timer1')
 print ("Readout took " + str(val) + " clock cycles")
 
 #Read overflow counter to confirm that the 40Gbe block did not overflow.
-of = brd.read_int('cosmic_ray_forty_g_tx_of')
+of = brd.read_int('forty_g_tx_of')
 overflows = of - previous_overflows
 if overflows == 0:
 	print("Packetizer never overflowed")
 else:
 	print("Packetizer overflowed" + str(val) + " times")
-afull = brd.read_int('cosmic_ray_forty_g_tx_afull')
+afull = brd.read_int('forty_g_tx_afull')
 new_afull = afull - previous_afull
 print("Ethernet block almost-full counter incremented by " + str(new_afull))
