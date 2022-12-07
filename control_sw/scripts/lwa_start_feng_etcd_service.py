cf3661c7c92f266bf59720ada8b29eda9e6e5673
Casey Law <caseyjlaw@gmail.com>
2022-12-07 18:56:33 +0000

    Turn on polling by default, with cadence 60s

--- control_sw/scripts/lwa_start_feng_etcd_service.py
+++ control_sw/scripts/lwa_start_feng_etcd_service.py
@@ -13,7 +13,7 @@ def main():
                         help ='Host serving etcd')
     parser.add_argument('-p', dest='port', type=int, default=2379,
                         help ='Port on which etcd is served')
-    parser.add_argument('-t', dest='polltime', type=int, default=None,
+    parser.add_argument('-t', dest='polltime', type=int, default=60,
                         help ='Time, in seconds, between FPGA status polls. '
                               'If None, don\'t start poll loop')
     parser.add_argument('--snaphost', dest='snaphost', type=str, default='snap01',
