39699133bc9603dfb003a2efd17158589877c3e3
Jack <jack@realtimeradio.co.uk>
2022-12-14 18:29:05 +0000

    v6 firmware: includes flagging
    Add "mask" block, which allows spectra to be blanked
    if their total power exceeds some user-settable threshold.
    Also provides tracking of how many spectra have been blanked
    per user-settable window (assumed to be the same as the correlator
    integration length)
    
    Also update FFT to downshift by 1 bit prior to FFT. This guards against
    possible overflow in the first FFT stage

--- fft_4096c_1i_core/ip_catalog/fft_4096c_1i_core.srcs/sources_1/bd/fft_4096c_1i_core_bd/synth/fft_4096c_1i_core_bd.vhd
+++ fft_4096c_1i_core/ip_catalog/fft_4096c_1i_core.srcs/sources_1/bd/fft_4096c_1i_core_bd/synth/fft_4096c_1i_core_bd.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2019.1.3 (lin64) Build 2644227 Wed Sep  4 09:44:18 MDT 2019
---Date        : Sat Aug  7 14:33:59 2021
---Host        : rtr-dev1 running 64-bit Ubuntu 18.04.4 LTS
+--Date        : Wed Dec  7 16:55:55 2022
+--Host        : rtr-dev1 running 64-bit Ubuntu 18.04.6 LTS
 --Command     : generate_target fft_4096c_1i_core_bd.bd
 --Design      : fft_4096c_1i_core_bd
 --Purpose     : IP block netlist
