accd0f2055835364a2339587abba5ae9de000bdf
Casey Law <caseyjlaw@gmail.com>
2022-12-21 11:45:34 +0000

    Add option to skip programming
    Much faster when it is safe to assume the boards are already programmed

--- control_sw/scripts/lwa_observe_etcd.py
+++ control_sw/scripts/lwa_observe_etcd.py
@@ -7,13 +7,14 @@ CONFIG_FILE = "/home/ubuntu/proj/lwa-shell/caltech-lwa/control_sw/config/lwa_cor
 SNAP_IDS = ','.join(['%d' % i for i in range(1,12)])
 GPU_IDS = ','.join(['%d' % i for i in range(1,9)])
 
-def configure_snaps(snapids, config_file):
+def configure_snaps(snapids, config_file, program=True):
     from lwa_f import snap2_feng_etcd_client
 
     ec = snap2_feng_etcd_client.Snap2FengineEtcdControl()
 
-    ec.send_command(0, 'feng', 'program', timeout=60*7, n_response_expected=11)
-    ec.send_command(0, 'feng', 'initialize', kwargs={'read_only':False}, timeout=60*5, n_response_expected=11)
+    if program:
+        ec.send_command(0, 'feng', 'program', timeout=60*7, n_response_expected=11)
+        ec.send_command(0, 'feng', 'initialize', kwargs={'read_only':False}, timeout=60*5, n_response_expected=11)
 
     for i in sorted(snapids):
         print("Initializing board %d" % i)
@@ -43,10 +44,12 @@ def main():
                         help ='Comma-separated list of SNAP ID numbers to initialize')
     parser.add_argument('-j', dest='gpuids', type=str, default=GPU_IDS,
                         help ='Comma-separated list of GPU server ID numbers to initialize')
+    parser.add_argument('--skipprog', dest='skipprog', action='store_true',
+                        help ='Use this flag to skip programming and ADC link training')
     args = parser.parse_args()
 
     if args.initsnaps:
-        configure_snaps(map(int, args.snapids.split(',')), args.configfile)
+        configure_snaps(map(int, args.snapids.split(',')), args.configfile, program=not args.skipprog)
 
     if args.initgpus:
         configure_gpu_servers(map(int, args.gpuids.split(',')))
