dbf98b1872f6d4a4e73e24bdd497bd20edc115a8
kathrynplant <plant.kathryn.a@gmail.com>
2023-04-17 00:37:06 +0000

    The version check script was skipping snap03, now snap03 is included.

--- cosmic_ray_scripts/snap2_control/quick_version_check.py
+++ cosmic_ray_scripts/snap2_control/quick_version_check.py
@@ -1,7 +1,7 @@
 import casperfpga
 import lwa_f
 
-for hostname in ['snap01','snap02','snap02','snap04','snap05','snap06','snap07','snap08','snap09','snap10','snap11']:
+for hostname in ['snap01','snap02','snap03','snap04','snap05','snap06','snap07','snap08','snap09','snap10','snap11']:
     brd=casperfpga.CasperFpga(hostname)
     lwaf=lwa_f.blocks.fpga.Fpga(brd,'')
     print(hostname, ' is running version ', lwaf.get_firmware_version())
