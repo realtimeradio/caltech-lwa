63387bf528ed65d98d2077c5b9aedbcd54fa8fd1
ubuntu <ubuntu@pm.me>
2023-07-02 05:49:09 +0000

    Added argument to datacapture.py to delay start.

--- cosmic_ray_scripts/data_capture/script_for_datacapture_to_2_drives.sh
+++ cosmic_ray_scripts/data_capture/script_for_datacapture_to_2_drives.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #This is a script to run datacapture.py with data saved to data0 until it's nearly full and then stop and run datacapture to data1
-./datacapture.py gpu09 overnight /data0/cosmic-ray-data/2023June20/ 704000 60 417479744
-./datacapture.py gpu09 overnight /data1/cosmic-ray-data/2023June20/ 704000 60 417479744
+./datacapture.py gpu09 overnight /data0/cosmic-ray-data/2023June30/ 704000 60 417479744
+./datacapture.py gpu09 overnight /data1/cosmic-ray-data/2023June30/ 704000 60 417479744
