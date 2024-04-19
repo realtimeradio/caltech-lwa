e8f377540e39b12a28659359f6db46eff75228e7
ubuntu <ubuntu@pm.me>
2024-04-19 00:28:23 +0000

    Versions from recent observing

--- cosmic_ray_scripts/data_capture/script_for_datacapture_to_2_drives.sh
+++ cosmic_ray_scripts/data_capture/script_for_datacapture_to_2_drives.sh
@@ -2,9 +2,6 @@
 
 #This is a script to run datacapture.py with data saved to data0 until it's nearly full and then stop and run datacapture to data1
 
-#old
-#./datacapture.py gpu09 overnight /data0/cosmic-ray-data/2023June30/ 704000 60 417479744
-#./datacapture.py gpu09 overnight /data1/cosmic-ray-data/2023June30/ 704000 60 417479744
 
-./datacapture.py gpu09 overnight /data0/cosmic-ray-data/2024February22/ 704000 60 417479744 6000 
-./datacapture.py gpu09 overnight /data1/cosmic-ray-data/2024February22/ 704000 60 417479744 6000 
+./datacapture.py gpu09 overnight /data0/cosmic-ray-data/newdata/ 704000 60 417479744 13500 
+./datacapture.py gpu09 overnight /data1/cosmic-ray-data/newdata/ 704000 60 417479744 0 
