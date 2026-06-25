117d910746b81c3b7095fe8902c446f50311fad0
jaycedowell <jaycedowell@gmail.com>
2026-06-25 14:38:49 -0600

    Require the adc_clocksource to be zero.

--- control_sw/src/zcu102_fengine.py
+++ control_sw/src/zcu102_fengine.py
@@ -699,8 +699,8 @@ class ZCU102Fengine():
 
         """
         if program:
-            assert adc_clocksource in (0, 1), \
-                "adc_clocksource needs to be either 0 or 1"
+            assert adc_clocksource == 0 \
+                "adc_clocksource needs to be 0"
             self.program()
             try:
                 self.adc.initialize(read_only=False, clocksource=adc_clocksource)
